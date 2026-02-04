bcec3caaae44f0982542af2fdc6026a08b419a12
OleksandrSemenov <ipz241_sop@student.ztu.edu.ua>
2026-02-04 16:48:40 +0200

    fixed-comparing-to-enum

--- ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc9c2542108e18e804e970e5384de9d292c7601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad8b1bc310d5282bf112366429715b987a19620")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
